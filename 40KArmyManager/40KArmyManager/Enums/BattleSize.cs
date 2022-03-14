60b41a5bd3bcb754af70744c23a9e3f88a675b74
Tomo-Shortty <tomo_007@live.com.au>
2022-03-14 22:58:52 +1000

    Added enums

---
+++ 40KArmyManager/40KArmyManager/Enums/BattleSize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40KArmyManager.Enums
+{
+    internal enum BattleSize
+    {
+        CombatPatrol,
+        Incursion,
+        StrikeForce,
+        Onslaught
+    }
+}
---
+++ 40KArmyManager/40KArmyManager/Enums/BattlefieldRole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40KArmyManager.Enums
+{
+    internal enum BattlefieldRole
+    {
+        HQ,
+        Troops,
+        Elites,
+        FastAttack,
+        Flyer,
+        HeavySupport,
+        Fortification,
+        DedicatedTransport,
+        LordOfWar
+    }
+}
